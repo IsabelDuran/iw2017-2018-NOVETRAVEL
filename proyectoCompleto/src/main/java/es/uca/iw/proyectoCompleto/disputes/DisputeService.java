8ac6a2bcbe41a91f21a0a7cfbe8a0f0f22385525
IsabelDuran <isabel.duranchumillas@alum.uca.es>
2018-05-09 19:30:20 +0200

    Terminado DisputeService

--- proyectoCompleto/src/main/java/es/uca/iw/proyectoCompleto/disputes/DisputeService.java
+++ proyectoCompleto/src/main/java/es/uca/iw/proyectoCompleto/disputes/DisputeService.java
@@ -1,5 +1,41 @@
 package es.uca.iw.proyectoCompleto.disputes;
 
-public class DisputeService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DisputeService
+{	
+	@Autowired
+	private DisputeRepository repository_;
+	
+	public Dispute loadDisputeByDisputeId(String disputeId)
+	{
+		Dispute dispute_ = repository_.findById(disputeId);
+		if(dispute_ == null)
+		{
+			//throw new DisputenameNotFoundException(bookingId);
+		}
+		return dispute_;
+	}
+	
+	public Dispute save(Dispute dispute_)
+	{
+		return repository_.save(dispute_);
+	}
+	
+	public Dispute findOne(Long arg0) {
+		
+		return repository_.findOne(arg0);
+	}
+	
+	public void delete(Dispute arg0)
+	{
+		repository_.delete(arg0);
+	}
+	
+	public List<Dispute> findAll()
+	{
+		return repository_.findAll();
+	}
 }
