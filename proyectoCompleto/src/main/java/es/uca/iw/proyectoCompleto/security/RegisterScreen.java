dcba24707186072f772a66131dc09397863623e3
peri <josemanuel.perinanfreire@alum.uca.es>
2018-05-19 22:28:44 +0200

    Registro aún no funcional

--- proyectoCompleto/src/main/java/es/uca/iw/proyectoCompleto/security/RegisterScreen.java
+++ proyectoCompleto/src/main/java/es/uca/iw/proyectoCompleto/security/RegisterScreen.java
@@ -2,44 +2,104 @@ package es.uca.iw.proyectoCompleto.security;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.data.Binder;
+import com.vaadin.data.converter.StringToIntegerConverter;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewDisplay;
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.spring.annotation.SpringViewDisplay;
+import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 import es.uca.iw.proyectoCompleto.Navbar;
+import es.uca.iw.proyectoCompleto.apartments.ApartmentManagementView;
+import es.uca.iw.proyectoCompleto.apartments.ApartmentView;
+import es.uca.iw.proyectoCompleto.reports.Report;
+import es.uca.iw.proyectoCompleto.reports.ReportEditor.ChangeHandler;
 import es.uca.iw.proyectoCompleto.users.User;
+import es.uca.iw.proyectoCompleto.users.UserEditor;
+import es.uca.iw.proyectoCompleto.users.UserService;
 
-public class RegisterScreen extends VerticalLayout
+@SpringComponent
+@UIScope
+public class RegisterScreen extends VerticalLayout implements View
 {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	
+	private User user;
+	
+	private final UserService se = new UserService();
 
+	private Binder<User> binder = new Binder<>(User.class);
+    
+    TextField firstName = new TextField("Nombre:");
+	TextField lastName = new TextField("Apellidos:");
+	TextField username = new TextField("Nombre de usuario:");
+	PasswordField password = new PasswordField("Contraseña");
+	TextField zipcodee = new TextField("Codigo postal:");
+	TextField direccion = new TextField("Direccion");
+	
+	private static long serialVersionUID = 1L;
+	
+	Button save = new Button("Guardar");
+	
+	
+	
+	@Autowired
 	public RegisterScreen()
 	{
+		
         Navbar navbar_ = new Navbar(0);
         addComponent(navbar_);
+        
+        
+        
 		
-		TextField name = new TextField("Nombre:");
-		TextField lastname = new TextField("Apellidos:");
-		TextField username = new TextField("Nombre de usuario:");
-		TextField password = new TextField("Contraseña: ");
+		addComponents(firstName, lastName, username, password, direccion, zipcodee);
+		addComponent(save);
 		
-		addComponent(name);
-		addComponent(lastname);
-		addComponent(username);
-		addComponent(password);
+		binder.forField(firstName).bind(User::getFirstName, User::setFirstName);
+		binder.forField(lastName).bind(User::getLastName,User::setLastName);
+		binder.forField(username).bind(User::getUsername,User::setUsername);
+		binder.forField(password).bind(User::getPassword, User::setPassword);
+		binder.forField(zipcodee).withConverter(new StringToIntegerConverter("Introducir un numero")).bind(User::getZipcode, User::setZipcode);
+		binder.forField(direccion).bind(User::getAddress, User::setAddress);
 		
+		save.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+		
+		binder.readBean(user);
+		
+		save.addClickListener(ev-> se.save(user));
+	
 		setMargin(false);
-		setSpacing(true);
+		setSpacing(true); 
 		
 	}
+	
+	
 	
 	@PostConstruct
 	void init()
 	{
 		
+	}
+	
+
+	public interface ChangeHandler {
+
+		void onChange();
 	}
 	
 	private void addUser()
@@ -49,5 +109,11 @@ public class RegisterScreen extends VerticalLayout
 				"username", 
 				"address", 
 				1111); 
+	}
+	
+	public void setChangeHandler(ChangeHandler h) {
+		// ChangeHandler is notified when either save or delete
+		// is clicked
+		save.addClickListener(e -> h.onChange());
 	}
 }
