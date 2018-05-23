93b881df5c6085be22043da235cfa59314efb95c
IsabelDuran <isabel.duranchumillas@alum.uca.es>
2018-05-23 19:18:19 +0200

    Arreglados errores constructores Registered, Manager y Administrator

--- proyectoCompleto/src/main/java/es/uca/iw/proyectoCompleto/users/Manager.java
+++ proyectoCompleto/src/main/java/es/uca/iw/proyectoCompleto/users/Manager.java
@@ -17,13 +17,13 @@ public class Manager extends User{
 		// TODO Auto-generated constructor stub
 	}
 
-	public Manager(String firstName, String lastName, String address, int zipcode) {
-		super(firstName, lastName, address, zipcode);
+	public Manager(String firstName, String lastName, String address, int zipcode, String email) {
+		super(firstName, lastName, address, zipcode, email);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Manager(String firstName, String lastName, String username, String address, int zipcode) {
-		super(firstName, lastName, username, address, zipcode);
+	public Manager(String firstName, String lastName, String username, String address, int zipcode, String email) {
+		super(firstName, lastName, username, address, zipcode, email);
 		// TODO Auto-generated constructor stub
 	}
 
